93f03747640244a03b7cd3467a39fc92e2c53cd1
tim-dc <77545252+tim-dc@users.noreply.github.com>
2022-02-11 20:52:55 +0800

    SQL Query Test 1 (WORKING) + comments

--- index.js
+++ index.js
@@ -23,34 +23,41 @@ const async = require('hbs/lib/async');
 const mysql = require('mysql2/promise');
 
 async function case1() {        
- 
+    //  Set values to change (UPDATE)
+    const movie_id = 6;  // (Can be edited)
+    const movie_year = 1975  // (Can be edited)
 
-    const movie_id = 6;
-    const movie_year = 1975
+    // Connects to node 1
     const node1 = await mysql.createConnection(config.db1);
 
+    // makes sql read arrays as '?' https://github.com/sidorares/node-mysql2/blob/master/documentation/Extras.md
     node1.config.namedPlaceholders = true;
     
-
+    // Set Transaction Level 
     await node1.execute("SET TRANSACTION ISOLATION LEVEL READ COMMITTED");
     console.log("Isolation level is now 'READ COMMITTED'");
 
+    // Transaction Start
     await node1.beginTransaction();
 
     try{
+        // SQL Statement 1
         await node1.execute( "SELECT * FROM movies WHERE movie_id= :x", {x: movie_id}, (err,rows) => {
         });
 
         console.log("Selected movie_id = 6")
         
+        // SQL Statement 2
         await node1.execute("UPDATE movies SET movie_year = :x WHERE movie_id = :y", {x: movie_year, y:movie_id},  (err,rows) => {
         });
 
         console.log("Updated year to 1975");
 
+        // Commit to confirm Transaction
         await node1.commit();
 
     }catch (err) {
+        // Roll back Portion
         console.error(`Error Occured trying to fetch Case 1: ${err.message}`, err);
         node1.rollback();
         console.info('Rollback successful');
@@ -60,25 +67,9 @@ async function case1() {
     
 }
 
+// Call function
 (async function testCase1() {
     console.log(await case1());
     process.exit(0);
   })();
 
-
-/*
-// NODE 1 CONNECTION
-node1.connect(function(err) {
-    if (err) {
-    return console.error('error: ' + err.message);
-    }
-
-    // NODE 2 CONNECTION
-    console.log('Node2 is Active.');
-});
-
-// Query
-node1.execute("SELECT * FROM movies WHERE movie_id=?",[6],(err,result)=>{
-    console.log(result);
-});
-*/
