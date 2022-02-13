73d8d089ee7f53b4bc34324b22d4e94833004449
nicscheng <nicole_t_cheng@dlsu.edu.ph>
2022-02-14 05:41:09 +0800

    test node activation

--- public/js/index.js
+++ public/js/index.js
@@ -7,9 +7,47 @@ $(function(){
     
     $('#run-case1').on('click', function() {
         isoLevel = $('#isolation-level').val();
+
         var inputIso = document.getElementById("iso-level-one");
         inputIso.setAttribute('value', isoLevel);
+
         console.log("Running Case 1 with Isolation level - " + isoLevel);
+
+        var checkone = document.querySelector('#node1-switch').checked;
+        var checktwo = document.querySelector('#node2-switch').checked;
+        var checkthree = document.querySelector('#node3-switch').checked;
+
+        var inputNode1 = document.getElementById("check-node-one");
+        var inputNode2 = document.getElementById("check-node-two");
+        var inputNode3 = document.getElementById("check-node-three");
+
+        if(checkone)
+        {        
+            inputNode1.setAttribute('value', '1');
+        }
+        else
+        {
+            inputNode1.setAttribute('value', '0');
+        }
+
+        if(checktwo)
+        {        
+            inputNode2.setAttribute('value', '1');
+        }
+        else
+        {
+            inputNode2.setAttribute('value', '0');
+        }
+
+        if(checkthree)
+        {        
+            inputNode3.setAttribute('value', '1');
+        }
+        else
+        {
+            inputNode3.setAttribute('value', '0');
+        }
+
     });
 
     $('#run-case2').on('click', function() {
